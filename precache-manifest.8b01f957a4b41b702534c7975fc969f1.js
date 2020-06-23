25c1b2e4932107f89e4bab93414e101b8865fc16
Oly Timoshenko <o.b.timoshenko@gmail.com>
2020-06-23 20:58:48 +0300

    Updates

---
+++ precache-manifest.8b01f957a4b41b702534c7975fc969f1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d1c89ad95fa784359d09d7e0d316411",
+    "url": "/OlySpring1/instasport.git/index.html"
+  },
+  {
+    "revision": "561eaba74c89185e78cc",
+    "url": "/OlySpring1/instasport.git/static/css/main.db0db119.chunk.css"
+  },
+  {
+    "revision": "14048f9d365449039fe1",
+    "url": "/OlySpring1/instasport.git/static/js/2.cb6eb68e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/OlySpring1/instasport.git/static/js/2.cb6eb68e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "561eaba74c89185e78cc",
+    "url": "/OlySpring1/instasport.git/static/js/main.72c98302.chunk.js"
+  },
+  {
+    "revision": "97181f7f49f1bd283205",
+    "url": "/OlySpring1/instasport.git/static/js/runtime-main.4541fdc4.js"
+  },
+  {
+    "revision": "6184c2e979c6289e937226af3e251c07",
+    "url": "/OlySpring1/instasport.git/static/media/Checked3.6184c2e9.svg"
+  },
+  {
+    "revision": "2fee0e96dbe6d935bded6b55acba5646",
+    "url": "/OlySpring1/instasport.git/static/media/Refresh2.2fee0e96.svg"
+  }
+]);
